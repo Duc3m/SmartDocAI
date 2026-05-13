222ef6a051c28516a5f8bf3dc153661c373fc503
Duc3m <ducemtran44@gmail.com>
2026-05-13 16:02:52 +0700

    test()

--- src/data_access/vector_store.py
+++ src/data_access/vector_store.py
@@ -23,13 +23,15 @@ def load_vector_db(folder_path, embedding_model):
     """
     return FAISS.load_local(folder_path, embedding_model, allow_dangerous_deserialization=True)
 
-def get_retriever(vector_db, k=6):
+def get_retriever(vector_db, k=6, fetch_k=24):
     """
     Tạo retriever.
     """
     return vector_db.as_retriever(
-        search_type="similarity",
+        search_type="mmr",
         search_kwargs={
-            "k": k
+            "k": k,
+            "fetch_k": fetch_k,
+            "lambda_mult": 0.7
         }
     )
